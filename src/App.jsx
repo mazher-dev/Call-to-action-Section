50f991151f6c37a5fe20d037c3bf129a0531097f
MH Code <mazheriftikhar1@gmail.com>
2024-12-05 18:21:21 +0500

    5-Dec Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Cta1 from './Components/Cta1/Cta1'
+import Cta2 from './Components/Cta2/Cta2'
+import Cta3 from './Components/Cta3/Cta3'
+import Cta4 from './Components/Cta4/Cta4'
+import Cta5 from './Components/Cta5/Cta5'
+import Cta6 from './Components/Cta6/Cta6'
+import Cta7 from './Components/Cta7/Cta7'
+import Cta8 from './Components/Cta8/Cta8'
+import Cta9 from './Components/Cta9/Cta9'
+import Cta10 from './Components/Cta10/Cta10'
+import Cta11 from './Components/Cta11/Cta11'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Cta4 />
+      <Cta1 />
+      <Cta2 />
+      <Cta3 />
+      <Cta5 />
+      <Cta6 />
+      <Cta7 />
+      <Cta8 />
+      <Cta9 />
+      <Cta10 />
+      <Cta11 />
+    </div>
   )
 }
 
